94e1eb14263b87d6c4d1874994b6c4102dcc91dc
Ash <apillai@buffalo.edu>
2017-01-06 02:37:57 -0500

    Add files via upload
    Fragment java files

---
+++ app/src/main/java/com/rentmi/rentmi/fragment/ProfileFragment.java
@@ -0,0 +1,47 @@
+package com.rentmi.rentmi.fragment;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rentmi.rentmi.R;
+
+/**
+ * Created by apill on 12/26/2016.
+ */
+
+public class ProfileFragment extends Fragment {
+    private final String FRAGMENT_TITLE = "My Profile";
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment HomeFragment.
+     */
+    public static ProfileFragment newInstance()
+    {
+
+        return new ProfileFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(FRAGMENT_TITLE);
+
+        return view;
+    }
+
+}
